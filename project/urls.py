9c48054fb0ab7dea72e521708dd435146678b171
Alexandro Blanco <alex.blanco@icfolson.com>
2016-07-27 15:36:40 -0500

    Adding all the requirements for django-cms

--- project/urls.py
+++ project/urls.py
@@ -1,14 +1,21 @@
-from django.conf.urls import include, url
+from cms.sitemaps import CMSSitemap
+from django.conf.urls import *  # NOQA
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
+from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from django.conf.urls.i18n import i18n_patterns
 
-from welcome.views import index, health
+urlpatterns = i18n_patterns('',
+    url(r'^admin/', admin.site.urls),
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': {'cmspages': CMSSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
+    url(r'^', include('cms.urls')),
+                            )
 
-urlpatterns = [
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
-    url(r'^health$', health),
-    url(r'^admin/', include(admin.site.urls)),
-]
+# This is only needed when using runserver.
+if settings.DEBUG:
+    urlpatterns = i18n_patterns('',
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',  # NOQA
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+        ) + staticfiles_urlpatterns() + urlpatterns  # NOQA
